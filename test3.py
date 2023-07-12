84966d13ca201ad76bb2a55390edb23f54480bfc
yijiangli <yli556@jhu.edu>
2023-07-12 14:53:33 -0400

    ten images

--- test3.py
+++ test3.py
@@ -1,8 +1,35 @@
 from PIL import Image
 
-
 img = Image.open("/home/yli556/william/project/dataSet/cc3m/images/0_2901536091.jpg")
+img.save("image_1.png")
 
-img.save("image.png")
+print(1)
 
+img = Image.open("/home/yli556/william/project/dataSet/cc3m/images/1_3239086386.jpg")
+img.save("image_2.png")
 
+print(2)
+
+img = Image.open("/home/yli556/william/project/dataSet/cc3m/images/2_2859920462.jpg")
+img.save("image_3.png")
+
+img = Image.open("/home/yli556/william/project/dataSet/cc3m/images/3_3911723019.jpg")
+img.save("image_4.png")
+
+img = Image.open("/home/yli556/william/project/dataSet/cc3m/images/4_2074769043.jpg")
+img.save("image_5.png")
+
+img = Image.open("/home/yli556/william/project/dataSet/cc3m/images/5_397841235.jpg")
+img.save("image_6.png")
+
+img = Image.open("/home/yli556/william/project/dataSet/cc3m/images/6_3231921107.jpg")
+img.save("image_7.png")
+
+img = Image.open("/home/yli556/william/project/dataSet/cc3m/images/7_2259577525.jpg")
+img.save("image_8.png")
+
+img = Image.open("/home/yli556/william/project/dataSet/cc3m/images/8_2808259422.jpg")
+img.save("image_9.png")
+
+img = Image.open("/home/yli556/william/project/dataSet/cc3m/images/10_1326993852.jpg")
+img.save("image_10.png")
