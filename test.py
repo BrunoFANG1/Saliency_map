521b180fd586d89bcf373c9a2b9423db650db372
bruno <1324441676@qq.com>
2023-07-11 20:12:53 -0400

    works perfectly now

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import torch
 import CLIP.clip as clip
-from Util import get_saliency_word, get_saliency_map, create_dict
+from Util import get_saliency_word, get_saliency_map
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
